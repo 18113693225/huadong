58829f088e73235782acf72d6cd821f9fa7e077a
lijun <309150945@qq.com>
2016-12-20 10:51:15 +0800

    webView添加下载功能

--- app/src/main/java/com/demo/app/activity/UpdateActivity.java
+++ app/src/main/java/com/demo/app/activity/UpdateActivity.java
@@ -2,6 +2,8 @@ package com.demo.app.activity;
 
 import android.annotation.SuppressLint;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,23 +33,21 @@ public class UpdateActivity extends BaseActivity {
         initView();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            if (mWebView.canGoBack()) {
-                mWebView.goBack();
-            } else {
-                finish();
-            }
-        }
-        return true;
-    }
-
 
     @SuppressLint("SetJavaScriptEnabled")
     private void initView() {
         mWebView = (WebView) findViewById(R.id.news_web);
         mPrgBar = (ProgressBar) findViewById(R.id.prgBar);
+
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setDefaultTextEncodingName("UTF-8");
+        mWebSettings.setBuiltInZoomControls(false);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebView.loadUrl("http://app.qq.com/#id=detail&appid=1105648833");
+
+
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
@@ -67,17 +67,18 @@ public class UpdateActivity extends BaseActivity {
             }
         });
         mWebView.setWebViewClient(new WebViewClient() {
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+                if (url.endsWith(".apk")) {
+                    Uri uri = Uri.parse(url);
+                    Intent viewIntent = new Intent(Intent.ACTION_VIEW, uri);
+                    UpdateActivity.this.startActivity(viewIntent);
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
             }
         });
-        WebSettings mWebSettings = mWebView.getSettings();
-        mWebSettings.setJavaScriptEnabled(true);
-        mWebSettings.setDefaultTextEncodingName("UTF-8");
-        mWebSettings.setBuiltInZoomControls(false);
-        mWebView.loadUrl("http://app.qq.com/#id=detail&appid=1105648833");
     }
 
 
